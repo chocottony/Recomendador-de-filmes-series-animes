8978c7492f0086a5869f82b66ea6051458c11d22
Antonio <kraftoiko@gmail.com>
2026-06-30 21:08:38 -0300

    inicio do desenvolvimento

--- recomendador/app.py
+++ recomendador/app.py
@@ -1,8 +1,34 @@
+import requests
+from http import HTTPStatus
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def read_root():
-    return {'message': 'Hello, World!'}
+    if "https://api.themoviedb.org/3/search/multi?query=Naruto" == True:
+        return """
+        <html>
+            <head>
+                <title>Recomendador</title>
+            </head>
+            <body>
+                <h1>Welcome to My App</h1>
+                <p>Working!</p>
+            </body>
+        </html>
+        """
+    else: 
+        return """
+        <html>
+            <head>
+                <title>Recomendador</title>
+            </head>
+            <body>
+                <h1>Welcome to My App</h1>
+                <p>Not working!</p>
+            </body>
+        </html>
+        """
