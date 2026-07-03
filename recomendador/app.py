8776858dc5d25a3bb65fe68316fc0880c24b7e48
Antonio <kraftoiko@gmail.com>
2026-07-03 18:50:19 -0300

    adição da barra de busca

--- recomendador/app.py
+++ recomendador/app.py
@@ -7,12 +7,28 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 TMBD_API_KEY = 'a9b8d6d94ef62653e6760d2b7e14b2f8'
 
-
 @app.get('/items', status_code=HTTPStatus.OK, response_class=HTMLResponse)
-async def read_items(name: str):
+async def read_items(name: str = ''):
     content = ''
     url = 'https://api.themoviedb.org/3/search/multi'
     params = {'query': name, 'api_key': TMBD_API_KEY}
+    if name == '':
+        return f"""
+        <html>
+            <head>
+                <title>Recomendador</title>
+            </head>
+            <body>
+                <h1>Welcome to My App</h1>
+                <form action = '/items' method = 'get'>
+                    <label for = 'name'>Enter a name:</label>
+                    <input type = 'text' id = 'name' name = 'name'>
+                    <input type = 'submit' value = 'Search'>
+                </form>
+            </body>
+        </html>
+        """
+    
     response = requests.get(url, params=params)
     info = response.json()
 
@@ -30,18 +46,12 @@ async def read_items(name: str):
             </head>
             <body>
                 <h1>Welcome to My App</h1>
+                <form action = '/items' method = 'get'>
+                    <label for = 'name'>Enter a name:</label>
+                    <input type = 'text' id = 'name' name = 'name'>
+                    <input type = 'submit' value = 'Search'>
+                </form>
                 {content}
-            </body>
-        </html>
-        """
-    return f"""
-        <html>
-            <head>
-                <title>Recomendador</title>
-            </head>
-            <body>
-                <h1>Welcome to My App</h1>
-                {'Not Working!'}
             </body>
         </html>
         """
